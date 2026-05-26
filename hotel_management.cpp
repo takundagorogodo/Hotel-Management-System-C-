ec11c57c98db28043d787f09373dcc8dd95f8d0c
GOROGODO TAKUNDA LEONARD <gorogodotakundah26@gmail.com>
2026-05-26 21:57:27 +0530

    Update hotel_management.cpp

--- hotel_management.cpp
+++ hotel_management.cpp
@@ -149,9 +149,9 @@ void hotelManagement() {
     int quant, choice;
 
     int Qrooms = 0, Qpasta = 0, Qburger = 0, Qnoodles = 0, Qshake = 0, Qchicken = 0;
-    // Sold items
+    
     int Srooms = 0, Spasta = 0, Sburger = 0, Snoodles = 0, Sshake = 0, Schicken = 0;
-    // Revenue
+    
     int Total_rooms = 0, Total_pasta = 0, Total_burger = 0, Total_noodles = 0, Total_shake = 0, Total_chicken = 0;
 
     cout << "\n\t\tQuantity of items we have\n";
@@ -204,7 +204,7 @@ void hotelManagement() {
                 } else {
                     cout << "\n\tOnly " << Qpasta - Spasta << " pasta(s) remaining!\n";
                 }
-                break;   // *** break added ***
+                break;  
 
             case 3:
                 cout << "\nEnter your Burger quantity: ";
